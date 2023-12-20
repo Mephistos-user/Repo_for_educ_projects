dd641bbaf0fbff7e599b7096058ca0ce47382758
Mephistos <Mephistos.user@yandex.ru>
2023-12-20 15:17:03 +0300

    промежуточная фиксация

---
+++ project_landing_page/JS/script.js
@@ -0,0 +1,20 @@
+$(function() {
+    $("#item1").hover(function() {
+        $("#submenu1").slideToggle(500);
+    });
+});
+$(function() {
+    $("#item2").hover(function() {
+        $("#submenu2").slideToggle(500);
+    });
+});
+$(function() {
+    $("#item3").hover(function() {
+        $("#submenu3").slideToggle(500);
+    });
+});
+$(function() {
+    $("#item4").hover(function() {
+        $("#submenu4").slideToggle(500);
+    });
+});
