e6622ff424a8336b7015759f0661248dc7f6280a
Mephistos <Mephistos.user@yandex.ru>
2024-01-17 12:01:20 +0300

    коррекция размеров

--- project_landing_page/JS/script.js
+++ project_landing_page/JS/script.js
@@ -1,20 +1,45 @@
 $(function() {
-    $("#item1").hover(function() {
-        $("#submenu1").slideToggle(500);
+    $(".item1").hover(function() {
+        $(".submenu1").slideToggle(500);
     });
 });
 $(function() {
-    $("#item2").hover(function() {
-        $("#submenu2").slideToggle(500);
+    $(".item2").hover(function() {
+        $(".submenu2").slideToggle(500);
     });
 });
 $(function() {
-    $("#item3").hover(function() {
-        $("#submenu3").slideToggle(500);
+    $(".item3").hover(function() {
+        $(".submenu3").slideToggle(500);
     });
 });
 $(function() {
-    $("#item4").hover(function() {
-        $("#submenu4").slideToggle(500);
+    $(".item4").hover(function() {
+        $(".submenu4").slideToggle(500);
+    });
+});
+$(function() {
+    $(".navbar-icon").click(function() {
+        $(".navbar2").slideToggle(500);
+    });
+});
+$(function() {
+    $(".item5").click(function() {
+        $(".submenu5").slideToggle(500);
+    });
+});
+$(function() {
+    $(".item6").click(function() {
+        $(".submenu6").slideToggle(500);
+    });
+});
+$(function() {
+    $(".item7").click(function() {
+        $(".submenu7").slideToggle(500);
+    });
+});
+$(function() {
+    $(".item8").click(function() {
+        $(".submenu8").slideToggle(500);
     });
 });
