31456fc6260d7077b372f1d648dba02fcb5b5dce
Mephistos <Mephistos.user@yandex.ru>
2024-01-18 02:58:12 +0300

    адаптация размеров

--- project_landing_page/JS/script.js
+++ project_landing_page/JS/script.js
@@ -1,3 +1,4 @@
+// slider navbar 1440 and more
 $(function() {
     $(".item1").hover(function() {
         $(".submenu1").slideToggle(500);
@@ -18,9 +19,38 @@ $(function() {
         $(".submenu4").slideToggle(500);
     });
 });
+
+// slider navbar 1152-1440
 $(function() {
     $(".navbar-icon").click(function() {
         $(".navbar2").slideToggle(500);
+    });
+});
+$(function() {
+    $(".item5").click(function() {
+        $(".submenu5").slideToggle(500);
+    });
+});
+$(function() {
+    $(".item6").click(function() {
+        $(".submenu6").slideToggle(500);
+    });
+});
+$(function() {
+    $(".item7").click(function() {
+        $(".submenu7").slideToggle(500);
+    });
+});
+$(function() {
+    $(".item8").click(function() {
+        $(".submenu8").slideToggle(500);
+    });
+});
+
+// slider navbar 1152 fnd less
+$(function() {
+    $(".navbar-icon2").click(function() {
+        $(".nav2").slideToggle(500);
     });
 });
 $(function() {
