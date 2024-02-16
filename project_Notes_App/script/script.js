8bd9ee0ba5dbdac87f28606a8128be3d5f51c355
Mephistos <Mephistos.user@yandex.ru>
2024-02-16 20:50:11 +0300

    refactoring

--- project_Notes_App/script/script.js
+++ project_Notes_App/script/script.js
@@ -1,91 +1,80 @@
-//  window.addEventListener('load', function(){
-    // вывод заметок из localStorage в документ
-    function createDivNote(keyIndex, index) {
-        let note = document.querySelector('.list-notes');
-        let value = localStorage.getItem(keyIndex);
+// создание контейнеров с контентом из localstorage
+function createDivNote(keyIndex, index) {
+    // родительский контейнер
+    let note = document.querySelector('.list-notes');
+    let value = localStorage.getItem(keyIndex);
 
-        let divCont = document.createElement('div');
-        divCont.className = 'note-container';
-        divCont.id = 'div-cont' + keyIndex;
-        divCont.classList.add('p-animation');
-        divCont.classList.remove('show');
+    // вложенный контейнер
+    let divCont = document.createElement('div');
+    divCont.className = 'note-container';
+    divCont.id = 'div-cont' + keyIndex;
+    divCont.classList.add('p-animation');
+    divCont.classList.remove('show');
 
-        let divBtn = document.createElement('div');
-        divBtn.className = 'btn-container';
+    // контейнер с кнопками
+    let divBtn = document.createElement('div');
+    divBtn.className = 'btn-container';
 
-        let p = document.createElement('p');
-        p.id = 'paragraf' + keyIndex;
-        p.textContent = `${Number(index) + 1}: ${value}`;
+    // параграф
+    let p = document.createElement('p');
+    p.id = 'paragraf' + keyIndex;
+    p.textContent = `${Number(index) + 1}: ${value}`;
 
-        let inp = document.createElement('input');
-        inp.style = 'display: none';                
-        inp.id = 'inp-area' + keyIndex;
+    // скрытое поле для ввода
+    let inp = document.createElement('input');
+    inp.style = 'display: none';                
+    inp.id = 'inp-area' + keyIndex;
 
-        let btnEdit = document.createElement('button');
-        btnEdit.textContent = 'Edit';
-        btnEdit.id = 'btn-edit' + keyIndex;
-        btnEdit.setAttribute('onclick', 'editNote(this.id)');
+    // кнопки редактировать, удалить, скрытая сохранить
+    let btnEdit = document.createElement('button');
+    btnEdit.textContent = 'Edit';
+    btnEdit.id = 'btn-edit' + keyIndex;
+    btnEdit.setAttribute('onclick', 'editNote(this.id)');
 
-        let btnDelete = document.createElement('button');
-        btnDelete.textContent = 'Del';
-        btnDelete.id = 'btn-del' + keyIndex;
-        // btnDelete.onclick = "delNote()"; // способ не работает
-        btnDelete.setAttribute('onclick', 'delNote(this.id)');
+    let btnDelete = document.createElement('button');
+    btnDelete.textContent = 'Del';
+    btnDelete.id = 'btn-del' + keyIndex;
+    // btnDelete.onclick = "delNote()"; // способ не работает
+    btnDelete.setAttribute('onclick', 'delNote(this.id)');
 
-        let btnOk = document.createElement('button');
-        btnOk.textContent = 'Ok';
-        btnOk.id = 'btn-ok' + keyIndex;
-        btnOk.style = 'display: none;';
-        btnOk.setAttribute('onclick', 'saveNote(this.id)');
+    let btnOk = document.createElement('button');
+    btnOk.textContent = 'Ok';
+    btnOk.id = 'btn-ok' + keyIndex;
+    btnOk.style = 'display: none;';
+    btnOk.setAttribute('onclick', 'saveNote(this.id)');
 
-        // создаем контейнер с вложенными элементами
-        note.appendChild(divCont)
-            .append(p, inp, divBtn)
-            divBtn.append(btnEdit, btnOk, btnDelete);
+    // создаем контейнер с вложенными элементами
+    note.appendChild(divCont)
+        .append(p, inp, divBtn)
+        divBtn.append(btnEdit, btnOk, btnDelete);
+};
+
+// функция вывода
+function outputNote() { 
+    // получаем массив ключей и сортируем
+    let keysArr = Object.keys(localStorage);
+
+    // сортируем массив по возрастанию
+    keysArr = keysArr.sort(function(a, b) {
+        return a - b;
+        });
+
+    // вызываем последовательно циклично функцию создания контейнера
+    for (let i = 0; i < localStorage.length; i++) {
+        createDivNote(keysArr[i], i);
+
+        animation(keysArr[i])
     };
+};
+// вызываем функцию вывода
+outputNote();
 
-
-
-
-    
-
-
-
-
-
-
-    // функция вывода
-    function outputNote() { 
-
-        // получаем массив ключей и сортируем
-        let keysArr = Object.keys(localStorage);
-        // сортируем массив
-        keysArr = keysArr.sort(function(a, b) {
-            return a - b;
-            });
-        // вызываем последовательно циклично функцию создания контейнера
-        
-        for (let i = 0; i < localStorage.length; i++) {
-            // setTimeout(()=> createDivNote(keysArr[i], i), 500);
-            createDivNote(keysArr[i], i);
-
-            // let div_cont = document.getElementById('div-cont' + keysArr[i]);
-            // div_cont.classList.add = 'show';
-            // setTimeout(()=> animation(keysArr[i]), 1500);
-            animation(keysArr[i])
-        };
-
-    };
-    // setTimeout(()=> outputNote(), 500);
-    outputNote();
-
+// функция анимации
 function animation(keyIndex) {   
-    console.log('animation function');
     
     let div_cont = document.getElementById('div-cont' + keyIndex);
 
     if(div_cont.classList.contains('show')) {
-        // setTimeout(()=> div_cont.classList.remove('show'), 500);
 
         div_cont.classList.remove('show');
 
@@ -97,102 +86,33 @@ function animation(keyIndex) {
 
 document.addEventListener('DOMContentLoaded', function(){
 
-// функция добавления записи
-const addBtn = document.querySelector('.btn-add');
+    // функция добавления записи
+    const addBtn = document.querySelector('.btn-add');
 
-addBtn.addEventListener('click', add_note);
+    addBtn.addEventListener('click', add_note);
 
-function add_note() {
-    const inputValue = document.querySelector('#areaAdd').value;
+    function add_note() {
+        const inputValue = document.querySelector('#areaAdd').value;
 
-    console.log(inputValue);
-    let l;    
+        let l;    
 
-    if (inputValue == '') {
-        alert('Вы ничего не написали. Запись не сохранена.');
-    } else {
-
-        if (localStorage.length == 0) {
-            l = 0;
+        if (inputValue == '') {
+            alert('Вы ничего не написали. Запись не сохранена.');
         } else {
-            let keysArr = Object.keys(localStorage);
-            keysArr = keysArr.sort(function(a, b) {
-                return b - a;
-            });
-            l = +(keysArr[0]) + 1;
+            if (localStorage.length == 0) {
+                l = 0;
+            } else {
+                let keysArr = Object.keys(localStorage);
+                keysArr = keysArr.sort(function(a, b) {
+                    return b - a;
+                });
+                l = +(keysArr[0]) + 1;
+            };
+            localStorage.setItem(l, inputValue);
+
+            createDivNote(l, localStorage.length - 1);
         };
-        localStorage.setItem(l, inputValue);
-
-        createDivNote(l, localStorage.length - 1);
-
-
+        // вызываем функцию анимации через таймаут
+        setTimeout(()=> animation(l), 500);
     };
-    setTimeout(()=> animation(l), 500);
-};
-
- // функция анимации
-//  function animation() {   
-//     // получаем массив ключей и сортируем
-//     console.log('animation function');
-//     let keysArr = Object.keys(localStorage);
-//     console.log(keysArr);
-//     // сортируем массив
-//     keysArr = keysArr.sort(function(a, b) {
-//         return a - b;
-//         });
-//     console.log(keysArr);
-
-//     // вызываем последовательно циклично функцию создания контейнера
-//     for (let i = 0; i < localStorage.length; i++) { 
-//         // createDivNote(keysArr[i], i);
-//         console.log(i);
-//         let div_cont = document.getElementById('div-cont' + keysArr[i]);
-//         // let div_cont = document.querySelector('#div-cont' + keysArr[i]);
-//         console.log('div-cont' + keysArr[i]);
-//         // div_cont.classList.add('p-animation');
-
-//         // div_cont.classList.remove('show');
-//         div_cont.classList.add('show');
-//     };
-// };
-
-// animation();
-
-
-// function animation(keyIndex) {   
-//     console.log('animation function');
-    
-//     // let div_cont = document.getElementById('div-cont' + keysArr[i]);
-//     let div_cont = document.getElementById('div-cont' + keyIndex);
-//     // let div_cont = document.querySelector('#div-cont' + keysArr[i]);
-
-//     // console.log('div-cont' + keysArr[i]);
-//     console.log('div-cont' + keyIndex);
-//     if('show' in div_cont.classList) {
-//         div_cont.classList.remove('show');
-
-//     } else {
-//         div_cont.classList.add('show');
-
-//     };
-
-
-// };
-
-
-// let keysArr = Object.keys(localStorage);
-// // сортируем массив
-// keysArr = keysArr.sort(function(a, b) {
-//     return a - b;
-//     });
-
-
-// for (let i = 0; i < localStorage.length; i++) {
-//     setTimeout(()=> animation(keysArr[i]), 1500);
-//     // animation(keysArr[i]);
-// };
-
-
-
-
 });
---
+++ project_Notes_App/script/script_buttons.js
@@ -0,0 +1,61 @@
+// функция удаления
+function delNote(id) {
+    // получаем число из idName
+    let cn = id.match(/\d/g).join('');
+    
+    if(confirm('Вы уверены, что хотите удалить запись?')) {
+        animation(cn);
+        setTimeout(()=> document.getElementById(id).parentElement.parentElement.remove(), 1500);
+        localStorage.removeItem(cn);
+    };
+};
+
+// функция редактирования
+function editNote(id) {
+    // получаем число из idName
+    let cn = id.match(/\d/g).join('');
+
+    // получаем родительский контейнер
+    let parent = document.getElementById(id).parentElement.parentElement;
+
+    let paragrafDel = parent.firstChild;
+    let btnDivDel = parent.lastChild;
+
+    // прячем параграф и кнопки
+    paragrafDel.style = 'display: none;';
+    btnDivDel.firstChild.style = 'display: none;';
+    btnDivDel.lastChild.style = 'display: none;';
+
+    // "достаем" поле для ввода и кнопку "Ок"
+    let inp = document.getElementById('inp-area' + cn);
+    inp.style = 'display: block;'
+    inp.value = localStorage.getItem(cn);
+
+    let btnOk = document.getElementById('btn-ok' + cn);
+    btnOk.style = 'display: block; background: green;'
+};
+
+// функция сохранения
+function saveNote(id) {
+
+    let cn = id.match(/\d/g).join('');
+
+    let btnOk = document.querySelector('#btn-ok' + cn);
+    btnOk.style = 'display: none;';
+
+    let btnEdit = document.querySelector('#btn-edit' + cn);
+    btnEdit.style = 'display: block;';
+
+    let btnDelete = document.querySelector('#btn-del' + cn);
+    btnDelete.style = 'display: block;';
+
+    let inputArea = document.querySelector('#inp-area' + cn);
+    inputArea.style = 'display: none;';
+
+    localStorage.setItem(cn, inputArea.value);
+
+    let p = document.querySelector('#paragraf' + cn);
+    p.style = 'display: block;';
+    p.textContent = `${+cn - 2}: ` + localStorage.getItem(cn);
+};
+
