e3d8ee03caccd7039a31b9f4ca85a2d61e1a5ffc
Mephistos <Mephistos.user@yandex.ru>
2024-09-01 17:55:17 +0300

    refactor

--- Praktika_4_semestr/Case_5/DjangoApp/form_of_greeting/form_of_greeting/greeting/forms.py
+++ Praktika_4_semestr/Case_5/DjangoApp/form_of_greeting/form_of_greeting/greeting/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+import re
 from django.utils.translation import gettext_lazy as _
 from .models import Names
 
@@ -14,8 +15,12 @@ class NameCreateForm(forms.ModelForm):
         #Проверка того, что имя не пустое
         if name == '':
             raise ValidationError(_('Имя не может быть пустым'))
-
+        if re.match(r'\d', name):
+            # raise ValueError('Имя не может начинатся с цифр')
+            raise ValidationError(_('Имя не может начинатся с цифр'))
+        
         return name
+    
     class Meta:
         model = Names
         fields = ['name', ]
