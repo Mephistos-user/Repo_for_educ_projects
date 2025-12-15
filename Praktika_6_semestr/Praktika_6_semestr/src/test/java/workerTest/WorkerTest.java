121cd661049a21e870b3b916b01e3390979b3e24
Mephistos <Mephistos.user@yandex.ru>
2025-12-15 17:56:20 +0300

    update docs

--- Praktika_6_semestr/Praktika_6_semestr/src/test/java/workerTest/WorkerTest.java
+++ Praktika_6_semestr/Praktika_6_semestr/src/test/java/workerTest/WorkerTest.java
@@ -1,7 +1,6 @@
 package worker;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -10,15 +9,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.mock;
 
 class WorkerTest {
+
     @Mock
     Worker testWorker1 = new Worker("testName1", "testPosition1", 110000.50, 2011);
+    @Mock
     Worker testWorker2 = new Worker("testName2", "testPosition2", 120000.50, 2012);
+    @Mock
     Worker testWorker3 = new Worker("testName3", "testPosition3", 130000.50, 2013);
+    @Mock
     Worker testWorker4 = new Worker("testName4", "testPosition4", 140000.50, 2014);
+    @Mock
     Worker testWorker5 = new Worker("testName5", "testPosition5", 150000.50, 2015);
     @Mock
     ArrayList<Worker> testWorkerList = new ArrayList<>();
@@ -36,28 +41,26 @@ class WorkerTest {
 
 
     @Test
-        // @DisplayName("Тест создания нового сотрудника")
     void createNewWorkerTest() {
-        Worker testWorker = new Worker();
+        Worker expected = new Worker();
         Scanner scannerMock = mock(Scanner.class);
         Mockito.when(scannerMock.nextLine()).thenReturn("testNameNew");
-        testWorker.setName(scannerMock.nextLine());
+        expected.setName(scannerMock.nextLine());
         Mockito.when(scannerMock.nextLine()).thenReturn("testPositionNew");
-        testWorker.setPosition(scannerMock.nextLine());
+        expected.setPosition(scannerMock.nextLine());
         Mockito.when(scannerMock.nextDouble()).thenReturn(250000.50);
-        testWorker.setSalary(scannerMock.nextDouble());
+        expected.setSalary(scannerMock.nextDouble());
         Mockito.when(scannerMock.nextInt()).thenReturn(2020);
-        testWorker.setYearOfAdmissionToWork(scannerMock.nextInt());
+        expected.setYearOfAdmissionToWork(scannerMock.nextInt());
 
-        Worker worker = new Worker("testNameNew", "testPositionNew", 250000.50, 2020);
-        worker.setId(testWorker.getId());
+        Worker actual = new Worker("testNameNew", "testPositionNew", 250000.50, 2020);
+        actual.setId(expected.getId());
 
-        assertEquals(worker, testWorker);
+        assertEquals(expected, actual);
     }
 
 
     @Test
-        // @DisplayName("Тест обновления данных")
     void updateWorkerTest() {
         HashMap<String, String> input = new HashMap<>();
         input.put("name", "updateName");
@@ -71,18 +74,43 @@ class WorkerTest {
 
         Worker.updateWorker(testWorkerList, input, idWorker);
 
-        Worker worker = new Worker("updateName", "updatePosition", 350000.50, 2022);
-        worker.setId(1);
+        Worker actual = new Worker("updateName", "updatePosition", 350000.50, 2022);
+        actual.setId(1);
 
-        assertEquals(worker, testWorkerList.getFirst());
+        assertEquals(testWorkerList.getFirst(), actual);
 
     }
-//
-//    @Test
-//    void deleteWorker() {
-//    }
-//
-//    @Test
-//    void getTopWorkersByStage() {
-//    }
+
+    @Test
+    void deleteWorker() {
+        int idWorker = 17;
+        boolean expected = testWorkerList.contains(Worker.getWorkerById(testWorkerList, idWorker));
+        Worker.deleteWorker(testWorkerList, idWorker);
+        boolean actual = testWorkerList.contains(Worker.getWorkerById(testWorkerList, idWorker));
+
+        assertNotEquals(expected, actual);
+    }
+
+    @Test
+    void getWorkerById() {
+        int testIdWorker = 11;
+        System.out.println(testWorkerList);
+        Worker expected = Worker.getWorkerById(testWorkerList, testIdWorker);
+
+        Worker actual = new Worker("testName3", "testPosition3", 130000.50, 2013);
+        actual.setId(testIdWorker);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getTopWorkersByStage() {
+        ArrayList<Worker> expected = Worker.getTopWorkersByStage(testWorkerList, 2013);
+        ArrayList<Worker> actual = new ArrayList<>();
+        actual.add(testWorker1);
+        actual.add(testWorker2);
+
+        assertEquals(expected, actual);
+
+    }
 }
